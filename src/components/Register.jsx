1bcf398eac490a495dcfd2e96c672d0fb7cedf7b
Farouk Goaudria <gouadriaafar@gmail.com>
2021-11-24 23:11:03 +0100

    isValid button Register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -15,6 +15,13 @@ export default class Register extends Component {
                 dateOfBirth: [],
                 password: [],
             },
+            message: "",
+            dirty: {
+                email: false,
+                username: false,
+                dateOfBirth: false,
+                password: false,
+            }
         };
     }
     render() {
@@ -26,17 +33,34 @@ export default class Register extends Component {
                     <div className='form-group form-row'>
                         <label className='col-lg-4 col-form-label' htmlFor='email'>Email</label>
                         <div className='col-lg-8'>
-                            <input type="text" id="email" className='form-control' value={this.state.email} onChange={(event) => {
-                                this.setState({ email: event.target.value }, this.validate);
-                            }} />
+                            <input type="text" id="email" className='form-control' autoFocus="autofocus" value={this.state.email} onChange={(event) => {
+                                let dirty = this.state.dirty;
+                                dirty.email = true;
+                                this.setState({ email: event.target.value, dirty: dirty }, this.validate);
+                            }}
+                                onBlur={(event) => {
+                                    let dirty = this.state.dirty;
+                                    dirty.email = true;
+                                    this.setState({ dirty: dirty }, this.validate);
+
+                                }}
+                            />
                         </div>
                     </div>
                     <div className='form-group form-row'>
                         <label className='col-lg-4 col-form-label' htmlFor='name'>UserName</label>
                         <div className='col-lg-8'>
                             <input type="text" id="name" className='form-control' value={this.state.username} onChange={(event) => {
+                                let dirty = this.state.dirty;
+                                dirty.username = true;
                                 this.setState({ username: event.target.value }, this.validate);
-                            }} />
+                            }}
+                                onBlur={(event) => {
+                                    let dirty = this.state.dirty;
+                                    dirty.username = true;
+                                    this.setState({ dirty: dirty }, this.validate);
+
+                                }} />
                         </div>
                     </div>
                     <br />
@@ -44,8 +68,17 @@ export default class Register extends Component {
                         <label className='col-lg-4 col-form-label' htmlFor='date'>Date Of BirthDay</label>
                         <div className='col-lg-8'>
                             <input type="date" id="date" className='form-control' value={this.state.dateOfBirth} onChange={(event) => {
+                                let dirty = this.state.dirty;
+                                dirty.dateOfBirth = true;
                                 this.setState({ dateOfBirth: event.target.value }, this.validate);
-                            }} />
+                            }}
+                                onBlur={(event) => {
+                                    let dirty = this.state.dirty;
+                                    dirty.dateOfBirth = true;
+                                    this.setState({ dirty: dirty }, this.validate);
+
+                                }}
+                            />
                         </div>
                     </div>
                     <br />
@@ -53,25 +86,41 @@ export default class Register extends Component {
                         <label className='col-lg-4 col-form-label' htmlFor='password'>Password</label>
                         <div className='col-lg-8'>
                             <input type="password" id="password" className='form-control' value={this.state.password} onChange={(event) => {
+                                let dirty = this.state.dirty;
+                                dirty.password = true;
                                 this.setState({ password: event.target.value }, this.validate);
-                            }} />
+                            }}
+                                onBlur={(event) => {
+                                    let dirty = this.state.dirty;
+                                    dirty.password = true;
+                                    this.setState({ dirty: dirty }, this.validate);
+
+                                }}
+                            />
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-lg-6">
+                            <div className='text-center'>
+                                {this.state.message}
+                            </div>
                             <button className="btn btn-primary m-2" onClick={this.OnRegisterClick}>
                                 Register
                             </button>
                         </div>
                         <ul className="text-danger">
                             {Object.keys(this.state.errors).map((control) => {
-                                return this.state.errors[control].map((err) => {
-                                    return <li key={err}>{err}</li>;
-                                });
+                                if (this.state.dirty[control]) {
+                                    return this.state.errors[control].map((err) => {
+                                        return <li key={err}>{err}</li>;
+                                    });
+                                } else {
+                                    return '';
+                                }
                             })}
                         </ul>
                         <div>
-                            {JSON.stringify(this.state.errors)}
+                            {JSON.stringify(this.state)}
                         </div>
                     </div>
                 </div>
@@ -140,4 +189,29 @@ export default class Register extends Component {
         //set errors
         this.setState({ errors: errors });
     };
+    isValid = () => {
+        let valid = true;
+        //read all controls from state.errors
+        for (let control in this.state.errors) {
+            if (this.state.errors[control].length > 0) {
+                valid = false;
+            }
+        }
+
+        return valid;
+    }
+    OnRegisterClick = () => {
+        this.validate();
+        var dirty = this.state.dirty;
+        Object.keys(dirty).forEach((control) => {
+            dirty[control] = true;
+
+        });
+        this.setState({ dirty: dirty });
+        if (this.isValid()) {
+            this.setState({ message: "valid" })
+        } else {
+            this.setState({ message: "Invalid" })
+        }
+    }
 }
